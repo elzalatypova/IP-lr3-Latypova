3709e2757333da2590354e5b5cabddde5d00d7cb
elzalatypova <79845168+elzalatypova@users.noreply.github.com>
2021-12-15 22:05:43 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Латыпова Эльза ПИ-323</title>
+</head>
+<body>
+	<a href="f1.html">Упражнение № 3-1 </a> <br>
+	<a href="f2.html">Упражнение № 3-2 </a> <br>
+	<a href="f2.2.html">Упражнение № 3-3 </a> <br>
+	<a href="f3.html">Упражнение № 3-4 </a> <br>
+	<a href="f5.html">Упражнение № 3-5 </a> <br>
+	<a href="f6.html">Упражнение № 3-6</a> <br>
+	<a href="f7.php">Упражнение № 3-7</a> <br>
+	<a href="s3-1.php">Задача 3.1</a> <br>
+	<a href="s3-2.php">Задача 3.2</a> <br>
+	<a href="s3-3.php">Задача 3.3</a> <br>
+	<a href="s3-4.php">Задача 3.4</a> <br>
+	<a href="s3-5.php">Задача 3.5</a> <br>
+	<a href="s3-6.html">Задача 3.6</a> <br>
+</body>
+</html>
